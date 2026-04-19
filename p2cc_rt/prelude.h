1c521cca5a145095964eacddef16579d92851c48
Danny Milosavljevic <dannym@friendly-machines.com>
2026-04-19 23:32:02 +0200

    Add popen.

--- p2cc_rt/prelude.h
+++ p2cc_rt/prelude.h
@@ -1983,7 +1983,14 @@ inline void p_exclude(Set<E1>& s, E2 v) {
 
 // STUB: misc small dos/system builtins used by the compiler sources.
 inline int32_t p_winstackpagesize = 4096;
-// STUB: `popen(f, cmd, mode)` opens a pipe to a process.
+inline void p_popen(TextFile& f, const ShortString<>& cmd, p_char mode) {
+  char m[2] = {static_cast<char>(std::tolower(p_char_to_c(mode))), '\0'};
+  f.f = ::popen(p_to_std_string(cmd).c_str(), m);
+  p_set_ioresult(f, f.f ? 0 : 5);
+}
+inline void p_popen(TextFile& f, const ShortString<>& cmd, char mode) {
+  p_popen(f, cmd, p_char_of(mode));
+}
 template <typename F>
 inline void p_popen(F&, const ShortString<>&, char) {}
 template <typename F>
