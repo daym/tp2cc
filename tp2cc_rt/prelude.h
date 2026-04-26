c48c7430fdd644c67d9f8c37f82de001f94295cd
Danny Milosavljevic <dannym@friendly-machines.com>
2026-04-26 21:20:55 +0200

    rt: Add faDirectory

--- tp2cc_rt/prelude.h
+++ tp2cc_rt/prelude.h
@@ -2905,6 +2905,7 @@ struct SearchRec { int32_t p_time = 0; int32_t p_size = 0;
                    std::size_t p_index = 0; };
 using p_searchrec = SearchRec;
 using p_tsearchrec = SearchRec;
+inline constexpr int32_t p_fadirectory = 0x10;
 inline void p_searchrec_fill(SearchRec& rec, const std::string& path) {
   struct stat st{};
   if (::stat(path.c_str(), &st) != 0) return;
